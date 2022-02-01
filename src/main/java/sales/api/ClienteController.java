420964607b93ef61b991ea944b323850b0310fcd
Aristevao <aristevao@hotmail.com>
2022-02-01 16:04:31 -0300

    [T-01] Request mapping args example

--- src/main/java/sales/api/ClienteController.java
+++ src/main/java/sales/api/ClienteController.java
@@ -8,7 +8,12 @@ public class ClienteController {
     public ClienteController() {
     }
 
-    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = "/hello/{name}",
+            method = RequestMethod.GET,
+            consumes = {"application/json", "application/xml"}, // request body
+            produces = {"application/json", "application/xml"}  // response body
+    )
     @ResponseBody
     public String helloCliente(@PathVariable("name") String name) {
         return String.format("Hello %s ", name);
