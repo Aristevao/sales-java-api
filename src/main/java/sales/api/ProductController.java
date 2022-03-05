963af72b88aa3440dbb0f609dfd6660173a75665
Aristevao <aristevao@hotmail.com>
2022-03-04 22:36:09 -0300

    [03-09] Save, update and find one by ID

--- src/main/java/sales/api/ProductController.java
+++ src/main/java/sales/api/ProductController.java
@@ -3,8 +3,11 @@ package sales.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import sales.domain.entity.Produto;
 import sales.domain.repository.ProductRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -20,5 +23,23 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public Produto save(@RequestBody Produto product) {
         return productRepository.save(product);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@RequestBody Produto product, @PathVariable Integer id) {
+        productRepository.findById(id)
+                .map(foundProduct -> {
+                    product.setId(id);
+                    productRepository.save(product);
+                    return foundProduct;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
+
+    @GetMapping("{id}")
+    public Produto findById(@PathVariable Integer id) {
+        return productRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
     }
 }
