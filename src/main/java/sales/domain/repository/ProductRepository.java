032b0f940719cb16ba12dfc7d96013d39d8954b2
Aristevao <68197280+Aristevao@users.noreply.github.com>
2022-03-24 07:44:29 -0300

    [03-09] Product (#8) (#9)
    * [03-09] Save product and remove useless files
    
    * [03-09] Save, update and find one by ID
    
    * [03-09] Get all
    
    * [03-09] Delete
    
    * [03-09] Rearranged response status

---
+++ src/main/java/sales/domain/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package sales.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sales.domain.entity.Produto;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Produto, Integer> {
+
+}
