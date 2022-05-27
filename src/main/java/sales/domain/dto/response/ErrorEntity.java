2d9798b2c897cf1f5fbdd175c0336b36f25acc40
Aristevao <aristevao@hotmail.com>
2022-05-27 15:28:39 -0300

    [03-14] Add advice to handle exceptions

---
+++ src/main/java/sales/domain/dto/response/ErrorEntity.java
@@ -0,0 +1,16 @@
+package sales.domain.dto.response;
+
+import lombok.Getter;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ErrorEntity {
+
+    @Getter
+    private List<String> errors;
+
+    public ErrorEntity(final String message) {
+        this.errors = Collections.singletonList(message);
+    }
+}
